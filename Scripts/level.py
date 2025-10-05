71dcb18fea4482d3b4a8a025d2b4268f7cb98abf
maverick1127 <228491248+smithuntharan-ops@users.noreply.github.com>
2025-10-05 22:35:09 +0530

    added door and test level2

--- Scripts/level.py
+++ Scripts/level.py
@@ -7,7 +7,6 @@ from classes import *
 class Level:
 
     def __init__(self):
-    
         self.world_data = {
             '0' : 'dirt',
             '1' : 'lava',
@@ -19,29 +18,27 @@ class Level:
             '7' : 'water', 
             '8' : 'waterdown',
             '9' : 'yplayer',
-            '10' : 'yyenemy'
+            '10' : 'yyenemy',
+            '11' : 'yyydoor'
         }
         
-        # Bg Update
         self.shiftx = 0
         self.shifty = 0
 
-        # List of elements
         self.tiles_in_map = []
-        self.solid_tiles = []  # Separate list for collision
-        self.hazard_tiles = []  # Fire and water tiles
+        self.solid_tiles = []
+        self.hazard_tiles = []
+        self.door_tiles = []
         self.player_obj = None
         self.enemy_manager = EnemyManager()
 
-        # Background
         self.background_img = pygame.image.load('Assets\\Background\\background.png')
         self.background_rec = self.background_img.get_rect()
         self.background_rec.topleft = (0, 0)
         self.started = False
         
-        # Hazard system
         self.respawn_invulnerability = 0
-        self.max_invulnerability = 60  # 1 second at 60 FPS
+        self.max_invulnerability = 60
         self.in_water = False
  
     def _draw_(self, data, surf):
@@ -50,59 +47,90 @@ class Level:
             
 
     def load_level(self, level):
-            with open(f'Assets\\Mapdata\\Level{level}.csv', 'r') as file:
-                data = list(reader(file))
+        self.tiles_in_map.clear()
+        self.solid_tiles.clear()
+        self.hazard_tiles.clear()
+        self.door_tiles.clear()
+        self.player_obj = None
+        self.enemy_manager.clear_enemies()
 
-                for y, row in enumerate(data):
-                    for x, tile in enumerate(row):
-                        if tile != ' ':
-                            if tile == '9':
-                                new_block = Player([x * 32, y * 32])
-                                self.player_obj = new_block
-                                self.player_obj.current_image_rec.topleft = (x * 32, y * 32)
-                            elif tile == '10':
-                                self.enemy_manager.add_enemy([x * 32, y * 32], 'slime')
-                            else:
-                                new_block = Tile((x * 32, y * 32), self.world_data[tile])
+        with open(f'Assets\\Mapdata\\Level{level}.csv', 'r') as file:
+            data = list(reader(file))
+
+            for y, row in enumerate(data):
+                for x, tile_id in enumerate(row):
+                    if tile_id != ' ':
+                        if tile_id == '9':
+                            new_block = Player([x * 32, y * 32])
+                            self.player_obj = new_block
+                            self.player_obj.current_image_rec.topleft = (x * 32, y * 32)
+                        elif tile_id == '10':
+                            self.enemy_manager.add_enemy([x * 32, y * 32], 'slime')
+                        else:
+                            if tile_id in self.world_data:
+                                tile_name = self.world_data[tile_id]
+                                new_block = Tile((x * 32, y * 32), tile_name)
                                 self.tiles_in_map.append(new_block)
                                 
-                                # Separate solid tiles from hazards
                                 if new_block.is_solid:
                                     self.solid_tiles.append(new_block)
+                                elif new_block.name == 'door':
+                                    self.door_tiles.append(new_block)
                                 else:
                                     self.hazard_tiles.append(new_block)
-            
+                            else:
+                                print(f"Warning: Unknown tile ID '{tile_id}' at ({x},{y}) in Level {level}. Ignoring.")
+        
+        if self.player_obj is None:
+            self.player_obj = Player([100, 100])
+            self.player_obj.current_image_rec.topleft = (100, 100)
+
 
     def find_nearest_safe_block(self):
         player_x = self.player_obj.pos.x
         player_y = self.player_obj.pos.y
         
-        # Look for solid blocks near player position
         nearest_block = None
         min_distance = float('inf')
         
-        for tile in self.solid_tiles:  # Only check solid tiles
-            # Check if block is above or near player
-            distance = abs(tile.rec.centerx - player_x)
+        
+        for tile in self.solid_tiles:
+           
+            x_distance = abs(tile.rec.centerx - player_x)
+            y_condition = tile.rec.top < player_y  
+            distance = x_distance + (player_y - tile.rec.top) * 0.1 
             
-            if distance < min_distance and tile.rec.top < player_y:
+            if y_condition and distance < min_distance:
                 min_distance = distance
                 nearest_block = tile
         
+        
+        if nearest_block is None:
+            for tile in self.solid_tiles:
+                if tile.rec.top < player_y: 
+                    nearest_block = tile
+                    break
+        
         if nearest_block:
-            # Spawn player on top of the block
+            
             self.player_obj.pos.x = nearest_block.rec.centerx - self.player_obj.current_image_rec.width // 2
             self.player_obj.pos.y = nearest_block.rec.top - self.player_obj.current_image_rec.height
-            self.player_obj.current_image_rec.topleft = (self.player_obj.pos.x, self.player_obj.pos.y)
+            self.player_obj.current_image_rec.topleft = (int(self.player_obj.pos.x), int(self.player_obj.pos.y))  
             self.player_obj.speed.x = 0
             self.player_obj.speed.y = 0
-            self.player_obj.gravity = 1
-            self.respawn_invulnerability = self.max_invulnerability
            
+            self.player_obj.land = True
+            self.player_obj.gravity = 0
+            self.respawn_invulnerability = self.max_invulnerability
         else:
+        
             self.player_obj.pos.x = 100
             self.player_obj.pos.y = 100
             self.player_obj.current_image_rec.topleft = (100, 100)
+            self.player_obj.speed.x = 0
+            self.player_obj.speed.y = 0
+            self.player_obj.land = False  
+            self.player_obj.gravity = 1
             self.respawn_invulnerability = self.max_invulnerability
 
     def check_hazards(self):
@@ -130,16 +158,13 @@ class Level:
                     self.in_water = True
 
     def update_backGround(self):
-        # Reset shifts
         self.shiftx = 0
         self.shifty = 0
         
-        # Horizontal camera
         player_x = self.player_obj.current_image_rec.x
         player_vx = self.player_obj.speed.x
         
         if player_x > 700 and self.background_rec.x > -960 and player_vx > 0:
-            # Match player speed 
             scroll = round(abs(player_vx))
             if scroll == 0 and abs(player_vx) > 0:
                 scroll = 1
@@ -157,7 +182,6 @@ class Level:
             self.shiftx = scroll
             self.player_obj.pos.x += scroll
         
-        # Vertical camera 
         player_y = self.player_obj.current_image_rec.y
         vertical_scroll_speed = 8
         
@@ -180,13 +204,14 @@ class Level:
         for tile in self.tiles_in_map:
             tile.rec.x += self.shiftx
             tile.rec.y += self.shifty
+        for tile in self.door_tiles:
+            tile.rec.x += self.shiftx
+            tile.rec.y += self.shifty
     
     def draw_player(self, surf):
         self.player_obj.draw_images(surf)
         
-        # Draw invulnerability indicator
         if self.respawn_invulnerability > 0:
-
             if self.respawn_invulnerability % 10 < 5:
                 shield_surf = pygame.Surface((self.player_obj.current_image_rec.width + 4, 
                                              self.player_obj.current_image_rec.height + 4), 
@@ -195,44 +220,37 @@ class Level:
                 surf.blit(shield_surf, (self.player_obj.current_image_rec.x - 2, 
                                        self.player_obj.current_image_rec.y - 2))
         
-       
         health_bar_x = 20
         health_bar_y = 20
         health_bar_width = 200
         health_bar_height = 20
         
-        # Background (dark red)
         pygame.draw.rect(surf, (100, 20, 20), 
                         (health_bar_x, health_bar_y, health_bar_width, health_bar_height))
         
-        # Current health (bright red to green gradient based on health)
         current_width = int((self.player_obj.lives / 5) * health_bar_width)
         if self.player_obj.lives > 3:
-            color = (50, 200, 50)  # Green
+            color = (50, 200, 50)
         elif self.player_obj.lives > 1:
-            color = (200, 200, 50)  # Yellow
+            color = (200, 200, 50)
         else:
-            color = (200, 50, 50)  # Red
+            color = (200, 50, 50)
         
         pygame.draw.rect(surf, color, 
                         (health_bar_x, health_bar_y, current_width, health_bar_height))
         
-        # Border
         pygame.draw.rect(surf, (255, 255, 255), 
                         (health_bar_x, health_bar_y, health_bar_width, health_bar_height), 2)
         
-        # Health text
         font = pygame.font.Font(None, 24)
         health_text = font.render(f"Lives: {self.player_obj.lives}/5", True, (255, 255, 255))
         surf.blit(health_text, (health_bar_x + 5, health_bar_y + 2))
 
     def collision_horizontal(self):
-        # Apply horizontal movement HERE
         self.player_obj.pos.x += self.player_obj.speed.x
         self.player_obj.current_image_rec.x = self.player_obj.pos.x
         
-        # Check horizontal collisions (only with solid tiles)
-        for tile in self.solid_tiles:  # Only check solid tiles
+        for tile in self.solid_tiles:
             if tile.rec.colliderect(self.player_obj.current_image_rec):
                 if self.player_obj.speed.x < 0:
                     self.player_obj.current_image_rec.left = tile.rec.right
@@ -245,16 +263,13 @@ class Level:
                 self.player_obj.speed.x = 0
 
     def collision_vertical(self):
-        # Apply vertical movement
         self.player_obj.pos.y += self.player_obj.speed.y
         self.player_obj.current_image_rec.y = self.player_obj.pos.y
         
         on_ground = False
         
-        # Check vertical collisions (only with solid tiles)
-        for tile in self.solid_tiles:  # Only check solid tiles
+        for tile in self.solid_tiles:
             if tile.rec.colliderect(self.player_obj.current_image_rec):
-                # Downward
                 if self.player_obj.speed.y > 0:
                     self.player_obj.current_image_rec.bottom = tile.rec.top
                     self.player_obj.pos.y = self.player_obj.current_image_rec.y
@@ -262,7 +277,6 @@ class Level:
                     self.player_obj.gravity = 0
                     on_ground = True
                 
-                # Upward
                 elif self.player_obj.speed.y < 0:
                     self.player_obj.current_image_rec.top = tile.rec.bottom
                     self.player_obj.pos.y = self.player_obj.current_image_rec.y
@@ -279,24 +293,21 @@ class Level:
                 self.shifty = -8
                 self.player_obj.pos[1] -= 8
                 self.update_Tiles()
-                # Also update enemy positions during startup
                 self.enemy_manager.update(0, self.shifty, self.solid_tiles)
 
     def start_level(self, surf):
-        # Background and Tiles
         surf.blit(self.background_img, self.background_rec)
         self._draw_(self.tiles_in_map, surf)
+        self._draw_(self.door_tiles, surf)
         self.level_startup()
         self.update_backGround()
         self.update_Tiles()
 
-        # Enemies - pass only solid tiles for collision
         self.enemy_manager.update(self.shiftx, self.shifty, self.solid_tiles)
         
-        # Player - apply water slowdown if needed
         original_max_speed = 5
         if self.in_water:
-            self.player_obj.max_speed = 2.5  # Half speed in water
+            self.player_obj.max_speed = 2.5
         else:
             self.player_obj.max_speed = original_max_speed
         
@@ -306,16 +317,17 @@ class Level:
         self.collision_horizontal()
         self.collision_vertical()
         
-        # Check hazards (fire/water)
         self.check_hazards()
+
+        for tile in self.door_tiles:
+            if self.player_obj.current_image_rec.colliderect(tile.rec):
+                return 'level_complete'
         
-        # Check enemy collisions
         collisions = self.enemy_manager.check_collisions(
             self.player_obj.current_image_rec, 
             self.player_obj.is_dashing
         )
         
-        # Handle collision results
         for collision_type in collisions:
             if collision_type == 'player_damaged' and self.respawn_invulnerability == 0:
                 self.player_obj.lives -= 1
@@ -324,6 +336,7 @@ class Level:
         if self.player_obj.lives <= 0:
             return 'game_over'
         
-        # Draw everything
         self.enemy_manager.draw(surf)
         self.draw_player(surf)
+        
+        return None
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,4 +1,5 @@
 import pygame
+import os  
 from level import Level
 from pause_menu import PauseMenu
 
@@ -8,7 +9,6 @@ screen = pygame.display.set_mode(SCREEN_RES)
 pygame.display.set_caption('Magic Wizard')
 clock = pygame.time.Clock()
 
-# Create pause menu
 pause_menu = PauseMenu(SCREEN_RES[0], SCREEN_RES[1])
 
 def load_level(level_num):
@@ -17,7 +17,6 @@ def load_level(level_num):
     return level
 
 def draw_game_over(surf):
-    # Semi-transparent red overlay
     overlay = pygame.Surface((SCREEN_RES[0], SCREEN_RES[1]))
     overlay.set_alpha(200)
     overlay.fill((20, 0, 0))
@@ -28,25 +27,73 @@ def draw_game_over(surf):
     center_x = SCREEN_RES[0] // 2
     center_y = SCREEN_RES[1] // 2 - 50
     
-    # Shadow (offset by 4 pixels down and right)
     shadow_text = title_font.render("GAME OVER", True, (100, 0, 0))
     shadow_rect = shadow_text.get_rect(center=(center_x + 4, center_y + 4))
     surf.blit(shadow_text, shadow_rect)
     
-    # Main text
     game_over_text = title_font.render("GAME OVER", True, (255, 50, 50))
     game_over_rect = game_over_text.get_rect(center=(center_x, center_y))
     surf.blit(game_over_text, game_over_rect)
     
-    # Instruction message
     instruction_font = pygame.font.Font(None, 40)
     instruction_text = instruction_font.render("Press ESC for menu", True, (255, 255, 255))
     instruction_rect = instruction_text.get_rect(center=(center_x, center_y + 100))
     surf.blit(instruction_text, instruction_rect)
 
+def draw_level_complete(surf, level_num):
+    overlay = pygame.Surface((SCREEN_RES[0], SCREEN_RES[1]))
+    overlay.set_alpha(200)
+    overlay.fill((0, 20, 0))
+    surf.blit(overlay, (0, 0))
+    
+    title_font = pygame.font.Font(None, 100)
+    
+    center_x = SCREEN_RES[0] // 2
+    center_y = SCREEN_RES[1] // 2 - 50
+    
+    shadow_text = title_font.render("LEVEL COMPLETE!", True, (0, 100, 0))
+    shadow_rect = shadow_text.get_rect(center=(center_x + 4, center_y + 4))
+    surf.blit(shadow_text, shadow_rect)
+    
+    level_complete_text = title_font.render("LEVEL COMPLETE!", True, (50, 255, 50))
+    level_complete_rect = level_complete_text.get_rect(center=(center_x, center_y))
+    surf.blit(level_complete_text, level_complete_rect)
+
+    instruction_font = pygame.font.Font(None, 40)
+    instruction_text = instruction_font.render(f"Press ENTER to proceed to Level {level_num + 1}", True, (255, 255, 255))
+    instruction_rect = instruction_text.get_rect(center=(center_x, center_y + 100))
+    surf.blit(instruction_text, instruction_rect)
+
+def draw_game_complete(surf):  
+    overlay = pygame.Surface((SCREEN_RES[0], SCREEN_RES[1]))
+    overlay.set_alpha(200)
+    overlay.fill((20, 20, 0)) 
+    surf.blit(overlay, (0, 0))
+    
+    title_font = pygame.font.Font(None, 100)
+    
+    center_x = SCREEN_RES[0] // 2
+    center_y = SCREEN_RES[1] // 2 - 50
+    
+    shadow_text = title_font.render("GAME COMPLETE!", True, (100, 100, 0))
+    shadow_rect = shadow_text.get_rect(center=(center_x + 4, center_y + 4))
+    surf.blit(shadow_text, shadow_rect)
+    
+    game_complete_text = title_font.render("GAME COMPLETE!", True, (255, 215, 0)) 
+    game_complete_rect = game_complete_text.get_rect(center=(center_x, center_y))
+    surf.blit(game_complete_text, game_complete_rect)
+    
+    instruction_font = pygame.font.Font(None, 40)
+    instruction_text = instruction_font.render("Press R to restart or ESC for menu", True, (255, 255, 255))
+    instruction_rect = instruction_text.get_rect(center=(center_x, center_y + 100))
+    surf.blit(instruction_text, instruction_rect)
+
+
 current_level = 1
 level_1 = load_level(current_level)
 game_over = False
+level_complete = False
+game_won = False  
 
 run = True
 while run:
@@ -59,26 +106,60 @@ while run:
         menu_action = pause_menu.handle_event(event)
         
         if menu_action == 'restart':
-            # Restart the level
             level_1 = load_level(current_level)
             pause_menu.is_paused = False
             game_over = False
+            level_complete = False
+            game_won = False  
         elif menu_action == 'quit':
             run = False
-    
+        
+        # Handle input for level complete screen
+        if level_complete and event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                next_level_file = f'Assets\\Mapdata\\Level{current_level + 1}.csv'
+                if os.path.exists(next_level_file): 
+                    current_level += 1
+                    level_1 = load_level(current_level)
+                    level_complete = False
+                    pause_menu.is_paused = False
+                else:
+                   
+                    game_won = True
+                    level_complete = False
+                    pause_menu.is_paused = True
+        
+        
+        if game_won and event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:  
+                current_level = 1
+                level_1 = load_level(current_level)
+                game_won = False
+                pause_menu.is_paused = False
+                game_over = False
+                level_complete = False
+            elif event.key == pygame.K_ESCAPE: 
+                pause_menu.is_paused = True  
+
     screen.fill((0, 0, 0))
     
-    if not pause_menu.is_paused:
+    if not pause_menu.is_paused and not level_complete and not game_won:
         result = level_1.start_level(screen)
         
-        # Check if game over
         if result == 'game_over':
             game_over = True
+            pause_menu.is_paused = True
+        elif result == 'level_complete':
+            level_complete = True
             pause_menu.is_paused = True
     
    
     if game_over:
         draw_game_over(screen)
+    elif level_complete:
+        draw_level_complete(screen, current_level)
+    elif game_won:  
+        draw_game_complete(screen)
     
     pause_menu.update()
     pause_menu.draw(screen)
